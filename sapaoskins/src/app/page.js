1af858523c568a3fd0cecf6b11f9add03058f1b7
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-22 19:32:56 -0300

    First Commit

---
+++ sapaoskins/src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <nav className="flex p-4 bg-slate-900">
+      <ul className="flex gap-20">
+        <li>
+          <a href="#">
+            <h1>Sapão Skins</h1>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Favoritos
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
